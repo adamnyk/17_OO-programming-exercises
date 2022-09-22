7aaf24dd6cba4b6ac191824bca0071f91b4a3d6b
Adam Pecan <adamnyk@gmail.com>
2022-09-22 14:22:01 -0600

    initial commit

---
+++ OO_Challenge/app.js
@@ -0,0 +1,66 @@
+// PART I
+class Vehicle {
+    constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    }
+    honk() {
+        return 'Beep.';
+    }
+    toString() {
+        return `${this.make} ${this.model} ${this.year}`;
+    }
+}
+
+let myPfinder = new Vehicle('Nissan', 'Pathfinder', 2004);
+
+
+// PART II
+class Car extends Vehicle {
+    constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+    }
+}
+
+let carosCRV = new Car('Honda', 'CRV', 1999);
+
+
+// PART III
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year)
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return 'VROOM!!!';
+    }
+}
+
+let carosMoto = new Motorcycle('Yamaha', 'V-Star 250', 2007);
+
+
+// PART IV
+class Garage {
+    constructor(capacity){
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+    add(vehicle) {
+        if (this.vehicles.length === this.capacity) {
+            return "Sorry, we're full.";
+        }
+        if (vehicle.__proto__.__proto__.name && 
+            vehicle.constructor.name !== 'Vehicle') {
+            return "Only vehicles are allowed in here!";
+        }
+        else {
+            this.vehicles.push(vehicle);
+            return 'Vehicle added!';
+        }
+    }
+}
+
+let myGarage = new Garage(2);
+let testCar = {name: 'testcar'};
